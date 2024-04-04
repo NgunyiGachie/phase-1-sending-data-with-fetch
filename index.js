95b3ad6307fcdce50066b7fe30d87e69a187b374
NgunyiGachie <antogachie@gmail.com>
2024-04-04 14:45:42 +0300

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+      method: 'POST',
+      headers: {
+        "Content-Type": 'application/json',
+        "Accept": 'application/json'
+      },
+      body: JSON.stringify({
+        name,
+        email
+      })
+    })
+    .then(resp => {
+      if (!resp.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return resp.json();
+    })
+    .then(function(json) {
+      document.body.innerHTML = `User ID: ${json.id}`;
+    })
+    .catch(error => {
+      document.body.innerHTML = `Error: ${error.message}`;
+    });
+  }
+  
+
